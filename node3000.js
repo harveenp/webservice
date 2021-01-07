1282460ed1b7bb73ad5622e7a9934f0db7fab485
harveen.punihani <harveen.punihani@fastshop.life>
2021-01-06 22:30:56 -0600

    api svc

---
+++ node3000.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const { parse } = require('querystring');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        collectRequestData(req, result => {
+            console.log(result);
+        
+        http.get('http://localhost:8080?age=${result.age}', (resp) => {
+          let data = '';
+          resp.on('data', (chunk) => {
+            data += chunk;
+            res.end(`received response is ${data}`);
+          })});
+
+
+            //res.end(`Parsed data belonging to ${result.fname}`);
+        });
+    } 
+    else {
+        res.end(`
+            <!doctype html>
+            <html>
+            <body>
+                <form action="/" method="post">
+                    <input type="text" name="fname" /><br />
+                    <input type="number" name="age" /><br />
+                    <input type="file" name="photo" /><br />
+                    <button>Save</button>
+                </form>
+            </body>
+            </html>
+        `);
+    }
+});
+server.listen(3000);
+
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
+}
